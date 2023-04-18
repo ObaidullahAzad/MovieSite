75e809297d63db4b1c010c4e1f78f6e5e3dd56b8
ObaidullahAzad <118367082+ObaidullahAzad@users.noreply.github.com>
2023-04-19 01:30:53 +0530

    Add files via upload

--- Funtion.js
+++ Funtion.js
@@ -1,3 +1,22 @@
-function myFunction(x){
-    x.classList.toggle("chevron-down-outline");
+const search = () =>{
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    console.log(searchbox);
+    const storeitems = document.getElementById("movielist")
+    const movie = document.querySelectorAll(".moviecard")
+    const pname = document.getElementsByTagName("a")
+
+    for(var i=0; i< pname.length; i++){
+        let match = movie[i].getElementsByTagName('a')[0];
+
+        if(match){
+            let textvalue = match.textContent || match.innerHTML
+
+            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+               movie[i].style.display = ""; 
+            } else{
+                movie[i].style.display="none";
+            }
+        }
+    }
+
 }
